bfddfdd4f741a22f4f1c4b28623779b076ba4930
Álvaro Ramos <78912452+AlvRM11@users.noreply.github.com>
2021-05-06 23:38:07 +0100

    Add files via upload

---
+++ CalculadoraTest.java
@@ -0,0 +1,36 @@
+import org.junit.*;
+
+public class CalculadoraTest {
+
+    @Test
+    public void TestSumayResta(){
+        Calculadora calculadora = new Calculadora(num1, num2);
+        
+    }
+
+    @Test
+    public void TestMultiplicacion(){
+        Calculadora calculadora = new Calculadora(num1, num2);
+    }
+
+    @Test
+    public void TestDivision(){
+        Calculadora calculadora = new Calculadora(num1, num2);
+    }
+
+    @Test
+    public void TestValorAbsoluto(){
+        Calculadora calculadora = new Calculadora(num1, num2);
+    }
+
+    @Test
+    public void TestExponente(){
+        Calculadora calculadora = new Calculadora(num1, num2);
+    }
+
+    @Test
+    public void TestEsPositivo(){
+        Calculadora calculadora = new Calculadora(num1, num2);        
+
+    }
+}
